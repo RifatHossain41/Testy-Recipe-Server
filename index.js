d455e02b680bc9344f45876bf190fbf736f35e3f
RifatHossain41 <rifathossain0423@gmail.com>
2023-05-04 16:45:35 +0600

    add bannar json data

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
-const port = 4000
+const cors = require('cors')
+const port = process.env.PORT || 4000;
+
+const chefs = require('./data/chef.json')
+const banner = require('./data/banner.json')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('food recipe is running')
+})
+
+app.get('/chefs', (req, res) => {
+  res.send(chefs)
+})
+
+app.get('/banner', (req, res) => {
+  res.send(banner)
 })
 
 app.listen(port, () => {
